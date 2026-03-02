ab62310ceb3a5bacfb4f5f238ae538edc2fab94b
darius1751 <lupequi12@gmail.com>
2026-03-02 17:34:17 -0500

    chore: use env's in backend cloudinary

--- backend/src/common/config/cloudinary.config.ts
+++ backend/src/common/config/cloudinary.config.ts
@@ -1,8 +1,8 @@
 import { v2 } from 'cloudinary';
 export const cloudinary = v2;
 cloudinary.config({
-    cloud_name: 'dnsct8obp',
-    api_key: '827292688682578',
-    api_secret: '3zSYIqrK5_BcJ7ULv-xPhtAHPcM',
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
     secure: true,
 })
